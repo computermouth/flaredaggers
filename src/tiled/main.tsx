05618b05a96928191b5c8c67d445831e9db48c77
computermouth <computermouth@crunchbangplusplus.org>
2021-08-21 17:48:13 -0700

    Updated map/tiles

--- src/tiled/main.tsx
+++ src/tiled/main.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="main" tilewidth="64" tileheight="32" spacing="1" margin="1" tilecount="4" columns="1">
- <image source="../../images/tilesets/main.png" width="65" height="132"/>
+<tileset version="1.4" tiledversion="1.4.2" name="main" tilewidth="64" tileheight="32" tilecount="5" columns="1">
+ <image source="../../images/tilesets/main.png" width="64" height="160"/>
 </tileset>
